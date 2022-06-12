46bab52ae3b13270cff6712506e8d963a53c90a7
Irfanullah Jan <irfanullah.jan@outlook.com>
2022-06-12 07:33:07 +0500

    food app navigation not working

--- 07/food-app/App.js
+++ 07/food-app/App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, View } from "react-native";
+import { createStackNavigator, } from "@react-navigation/stack";
+import Restaurants from "./components/Restaurants";
+import Menu from "./components/Menu";
 
 export default function App() {
+  // stack navigator is a container for the screens
+  // each screen is a component
+
+  const Stack = createStackNavigator();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SafeAreaView>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Menu">
+            <Stack.Screen name="Restaurants" component={Restaurants} />
+            <Stack.Screen name="Menu" component={Menu} />
+          </Stack.Navigator>
+        </NavigationContainer>
+        <StatusBar style="auto" />
+      </SafeAreaView>
     </View>
   );
 }
@@ -13,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
