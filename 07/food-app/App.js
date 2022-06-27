7332ca95c1bd7e026a7c2941d9da98ea85a3135e
Irfanullah Jan <irfanullah.jan@outlook.com>
2022-06-27 20:52:42 +0500

    final submission

--- 07/food-app/App.js
+++ 07/food-app/App.js
@@ -1,9 +1,8 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, View } from "react-native";
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
 import { createStackNavigator, } from "@react-navigation/stack";
-import Restaurants from "./components/Restaurants";
-import Menu from "./components/Menu";
+import { TableView, Cell, Section } from "react-native-tableview-simple";
 
 export default function App() {
   // stack navigator is a container for the screens
@@ -13,10 +12,10 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <SafeAreaView>
+      <SafeAreaView style={{ flex: 1 }}>
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="Restaurants">
-            <Stack.Screen name="Restaurants" component={Restaurants} />
+          <Stack.Navigator>
+            <Stack.Screen name="Restaurants" component={HomeScreen} />
             <Stack.Screen name="Menu" component={Menu} />
           </Stack.Navigator>
         </NavigationContainer>
@@ -26,11 +25,157 @@ export default function App() {
   );
 }
 
+function HomeScreen({ navigation }) {
+  const restaurants = [
+    {
+      title: "Joe's Gelato",
+      tagline: "Desert, Ice cream, £££",
+      eta: "10-30",
+      imgUri: require('./assets/joes-gelato.jpeg')
+    }, {
+      title: "Joe's Dinner",
+      tagline: "American, burgers, ££",
+      eta: "50+",
+      imgUri: require('./assets/joes-diner.jpeg')
+    }
+  ];
+
+  return (
+    <View>
+      <ScrollView>
+        <TableView>
+          <Section header="" hideSeparator={true}>
+            {restaurants.map(restaurant => (
+              <HomeScreenCell key={restaurant.title} {...restaurant} action={() => navigation.navigate("Menu", {
+                title: restaurant.title
+              })} />
+            ))}
+          </Section>
+        </TableView>
+      </ScrollView>
+    </View>
+  );
+}
+
+function HomeScreenCell(props) {
+  const { title, tagline, eta, imgUri, action } = props;
+  return (
+    <Cell
+      backgroundColor="transparent"
+      highlightUnderlayColor="#CCC"
+      cellContentView={
+        <View style={styles.cellContentView}>
+          <View style={styles.cellImageView}>
+            <Image source={imgUri} style={styles.cellImage} />
+          </View>
+          <View style={styles.cellTextView}>
+            <Text style={styles.cellTitle}>{title}</Text>
+            <Text style={styles.cellTagline}>{tagline}</Text>
+            <View style={styles.cellEta}>
+            <Text style={styles.cellEtaText}>{eta}{"\n"}mins</Text>
+            </View>
+          </View>
+        </View>
+      }
+      onPress={action}
+    />
+  )
+}
+
+function Menu(props) {
+  const { title } = props.route.params;
+  const menus = {
+    "Joe's Gelato": [
+      {
+        title: "Vanilla Ice Cream",
+        price: "££",
+      }, {
+        title: "Chocolate Ice Cream",
+        price: "££",
+      }, {
+        title: "Strawberry Ice Cream",
+        price: "££",
+      }
+    ],
+    "Joe's Dinner": [
+      {
+        title: "Steak",
+        price: "£££",
+      }, {
+        title: "Chicken",
+        price: "££",
+      }
+    ]
+  }
+
+  return (
+    <View>
+      <ScrollView>
+        <TableView>
+          <Section>
+            {menus[title].map(menu => (
+              <Cell key={menu.title} title={menu.title} />
+            ))}
+          </Section>
+        </TableView>
+      </ScrollView>
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
+  cellContentView: {
+
+    borderBottomColor: "#ccc",
+    backgroundColor: "none",
+    width: "100%",
+  },
+  cellImageView: {
+    width: "100%",
+    height: 290,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cellImage: {
+    width: "100%",
+    height: "100%",
+  },
+  cellTextView: {
+    padding: 10,
+    width: "100%",
+  },
+  cellTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  cellTagline: {
+    fontSize: 14,
+    color: "#666",
+  },
+  cellEta: {
+    position: "absolute",
+    right: 10,
+    top: -30,
+    backgroundColor: "#fff",
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    textAlign: "center",
+    borderRadius: 100000,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+  },
+  cellEtaText: {
+    fontSize: 14,
+    textAlign: "center",
+  }
 });
---
+++ README.md
@@ -0,0 +1,37 @@
+# Mobile Development
+
+## Expo links
+
+1. 
+2. [Noughts and Crosses](https://snack.expo.dev/@irfanjan/noughts-crosses)
+3. [Calculator](https://snack.expo.dev/@irfanjan/calculator)
+4. [Food App](https://snack.expo.dev/@irfanjan/food-app)
+
+
+### Name Badge
+
+Expo Snack: [https://snack.expo.dev/@irfanjan/name-badge](https://snack.expo.dev/@irfanjan/name-badge)
+
+![](./02/name-badge/Screenshot.png)
+
+### Hotel booking
+
+#### Wireframes
+
+| Home | Search | Detail | Setting |
+| ---- | ---- | ---- | ---- |
+| ![](./03/hotel-booking/01-home-screen.png) | ![](./03/hotel-booking/02-search-results.png) | ![](./03/hotel-booking/03-details.png) | ![](./03/hotel-booking/04-settings.png) |
+
+#### User flow
+
+![](./03/hotel-booking/05-user-flow-diagram.png)
+
+
+| Noughts and Crosses | Calculator | Food App |
+| ---- | ---- | ---- |
+| [Expo Snack](https://snack.expo.dev/@irfanjan/noughts-crosses) | [Expo Snack](https://snack.expo.dev/@irfanjan/calculator) | [Expo Snack](https://snack.expo.dev/@irfanjan/food-app) |
+| ![](./04/noughts-crosses/Screenshot.png) | ![](./05/calculator/Screenshot.png) | ![](./07/food-app/Screenshot.png) |
+
+
+
+
