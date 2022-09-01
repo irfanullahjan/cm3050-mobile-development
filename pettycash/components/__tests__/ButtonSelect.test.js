57b78becd88cf182ba741db376ce9c75133a67cf
Irfanullah Jan <irfanullah.jan@outlook.com>
2022-09-01 17:53:35 +0500

    button select test

--- pettycash/components/__tests__/ButtonSelect.test.js
+++ pettycash/components/__tests__/ButtonSelect.test.js
@@ -1,21 +1,46 @@
 import { useField } from "formik";
 import React from "react";
-import { render } from "react-native-testing-library";
+import { fireEvent, render } from "react-native-testing-library";
 
 import { ButtonSelect } from "../ButtonSelect";
 
 jest.mock("formik");
 
 describe("ButtonSelect", () => {
-  it("renders correctly", () => {
+  it("renders", () => {
     useField.mockReturnValue([
       { value: "test" },
-      { error: "test", touched: true },
+      { error: "", touched: false },
       { setValue: jest.fn(), setTouched: jest.fn() },
     ]);
     const tree = render(
-      <ButtonSelect name="test" options={[{ label: "test", value: "test" }]} />
+      <ButtonSelect
+        name="test"
+        options={[
+          { label: "test", value: "test" },
+          { label: "test2", value: "test2" },
+        ]}
+      />
     ).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+  it("selects correct option", () => {
+    const setValueHelper = jest.fn();
+    useField.mockReturnValue([
+      { value: "test" },
+      { error: "", touched: false },
+      { setValue: setValueHelper, setTouched: jest.fn() },
+    ]);
+    const { getByText } = render(
+      <ButtonSelect
+        name="test"
+        options={[
+          { label: "test", value: "test" },
+          { label: "test2", value: "test2" },
+        ]}
+      />
+    );
+    fireEvent.press(getByText("test2"));
+    expect(setValueHelper).toHaveBeenCalledWith("test2");
   });
 });
