ff02be57f24ff7c8f746f547785b73e0ce8d780f
Irfanullah Jan <irfanullah.jan@outlook.com>
2022-09-01 17:06:34 +0500

    more tests

--- pettycash/components/__tests__/TextInput.test.js
+++ pettycash/components/__tests__/TextInput.test.js
@@ -7,13 +7,53 @@ import { TextInput } from "../TextInput";
 jest.mock("formik");
 
 describe("TextInput", () => {
-  it("renders correctly", () => {
+  it("renders", () => {
+    useField.mockReturnValue([
+      { value: "" },
+      { error: "", touched: false },
+      { setValue: jest.fn(), setTouched: jest.fn() },
+    ]);
+    const tree = render(<TextInput name="inputName" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it("renders correctly with label", () => {
+    useField.mockReturnValue([
+      { value: "Input value" },
+      { error: "Input error", touched: false },
+      { setValue: jest.fn(), setTouched: jest.fn() },
+    ]);
+    const tree = render(
+      <TextInput name="inputName" label="Test label" />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it("renders correctly with placeholder", () => {
     useField.mockReturnValue([
       { value: "test" },
       { error: "test", touched: true },
       { setValue: jest.fn(), setTouched: jest.fn() },
     ]);
-    const tree = render(<TextInput name="test" />).toJSON();
+    const tree = render(
+      <TextInput name="test" placeholder="Test placeholder" />
+    ).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+  it("doesn't show validation error when untouched", () => {
+    useField.mockReturnValue([
+      { value: "Some value" },
+      { error: "required", touched: false },
+      { setValue: jest.fn(), setTouched: jest.fn() },
+    ]);
+    const { getByText } = render(<TextInput name="test" />);
+    expect(() => getByText("required")).toThrow();
+  });
+  it("shows validation error when touched", () => {
+    useField.mockReturnValue([
+      { value: "Some value" },
+      { error: "required", touched: true },
+      { setValue: jest.fn(), setTouched: jest.fn() },
+    ]);
+    const { getByText } = render(<TextInput name="test" />);
+    expect(getByText("required")).toBeTruthy();
   });
 });
