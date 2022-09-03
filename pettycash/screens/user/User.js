9619d9ad0a14c78a48324da58b88c66a3ce03afa
Irfanullah Jan <irfanullah.jan@outlook.com>
2022-09-03 11:29:38 +0500

    delete account and data feature, icons in user page

--- pettycash/screens/user/User.js
+++ pettycash/screens/user/User.js
@@ -1,11 +1,12 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { signOut } from "firebase/auth";
+import { deleteUser, signOut } from "firebase/auth";
+import { deleteDoc, doc } from "firebase/firestore";
 import { useTranslation } from "react-i18next";
-import { ScrollView, View } from "react-native";
+import { Image, ScrollView, View } from "react-native";
 import { Cell, Section, TableView } from "react-native-tableview-simple";
 
 import { themeTypes, useThemeContext } from "../../contexts/ThemeContext";
-import { auth } from "../../firebase";
+import { auth, firestore } from "../../firebase";
 import { alert } from "../../utils/alert";
 import { confirm } from "../../utils/confirm";
 
@@ -44,6 +45,45 @@ export function User({ navigation }) {
       });
   };
 
+  const confirmDeleteAccountAndData = () => {
+    confirm(
+      t("confirmDeleteAccount.title"),
+      t("confirmDeleteAccount.message"),
+      [
+        {
+          text: t("translation:cancel"),
+          style: "cancel",
+        },
+        {
+          text: t("confirmDeleteAccount.confirm"),
+          style: "destructive",
+          onPress: deleteAccountAndData,
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
+  const deleteAccountAndData = () =>
+    deleteDoc(doc(firestore, "users", auth.currentUser.uid))
+      .then(() => {
+        deleteUser(auth.currentUser)
+          .then(() => {
+            AsyncStorage.clear().catch((error) => console.error(error));
+            alert(t("accountDeleted"));
+          })
+          .catch((error) => {
+            alert(t("accountDeleteFailed"));
+            console.error(error);
+          });
+      })
+      .catch((error) => {
+        alert(t("accountDataDeleteFailed"));
+        console.error(error);
+      });
+
+  // Icons from https://iconscout.com
+
   return (
     <View style={{ flex: 1 }}>
       <ScrollView>
@@ -52,13 +92,23 @@ export function User({ navigation }) {
             header={`${t("currentlyLoggedInAs")} ${auth.currentUser.email}`}>
             <Cell
               cellStyle="Basic"
-              title={t("logout")}
-              onPress={() => confirmLogout()}
+              title={t("userDetail")}
+              onPress={() => navigation.navigate("User Detail", {})}
+              image={<Image source={require("../../assets/user/info.png")} />}
             />
             <Cell
               cellStyle="Basic"
-              title={t("userDetail")}
-              onPress={() => navigation.navigate("User Detail", {})}
+              title={t("deleteAccount")}
+              onPress={() => confirmDeleteAccountAndData()}
+              image={<Image source={require("../../assets/user/remove.png")} />}
+            />
+            <Cell
+              cellStyle="Basic"
+              title={t("logout")}
+              onPress={() => confirmLogout()}
+              image={
+                <Image source={require("../../assets/user/signout.png")} />
+              }
             />
           </Section>
         </TableView>
